d5927df5b558bb5e03f3f673aeb6bc4cda2efdaa
meltingong <hwk1210@naver.com>
2023-02-03 17:59:43 +0900

    ..

---
+++ web-practice/src/main/java/product/Category.java
@@ -0,0 +1,7 @@
+package product;
+
+public class Category {
+	private int ca_no; // 카테고리 번호
+	private String ca_name; // 카테고리 이름
+	
+}
--- web-practice/src/main/java/product/Product.java
+++ web-practice/src/main/java/product/Product.java
@@ -1,89 +1,15 @@
 package product;
 
 public class Product {
-	private int p_no;
-	private String p_name;
-	private int p_price;
-	private String p_desc;
-	private String category;
-	private String p_image;
-	private int p_click_count;
+	private int p_no; 			// 상품 번호
+	private String p_name;		// 상품 이름
+	private int p_price;		// 상품 가격
+	private String p_desc;		// 상품 설명
+	private String p_image;		// 상품 이미지
+	private int p_click_count;	// 상품 클릭 카운트
 	
 	public Product() {
 	
-	}
-
-	public Product(int p_no, String p_name, int p_price, String p_desc, String category, String p_image,
-			int p_click_count) {
-		this.p_no = p_no;
-		this.p_name = p_name;
-		this.p_price = p_price;
-		this.p_desc = p_desc;
-		this.category = category;
-		this.p_image = p_image;
-		this.p_click_count = p_click_count;
-	}
-
-	public int getP_no() {
-		return p_no;
-	}
-
-	public void setP_no(int p_no) {
-		this.p_no = p_no;
-	}
-
-	public String getP_name() {
-		return p_name;
-	}
-
-	public void setP_name(String p_name) {
-		this.p_name = p_name;
-	}
-
-	public int getP_price() {
-		return p_price;
-	}
-
-	public void setP_price(int p_price) {
-		this.p_price = p_price;
-	}
-
-	public String getP_desc() {
-		return p_desc;
-	}
-
-	public void setP_desc(String p_desc) {
-		this.p_desc = p_desc;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getP_image() {
-		return p_image;
-	}
-
-	public void setP_image(String p_image) {
-		this.p_image = p_image;
-	}
-
-	public int getP_click_count() {
-		return p_click_count;
-	}
-
-	public void setP_click_count(int p_click_count) {
-		this.p_click_count = p_click_count;
-	}
-
-	@Override
-	public String toString() {
-		return "Product [p_no=" + p_no + ", p_name=" + p_name + ", p_price=" + p_price + ", p_desc=" + p_desc
-				+ ", category=" + category + ", p_image=" + p_image + ", p_click_count=" + p_click_count + "]";
 	}
 
 
