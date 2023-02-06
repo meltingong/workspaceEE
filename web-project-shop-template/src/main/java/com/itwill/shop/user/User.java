b9cd04822cdf16fb0d839e99599bf9d81061d239
meltingong <hwk1210@naver.com>
2023-02-06 12:02:25 +0900

    ..

--- web-project-shop-template/src/main/java/com/itwill/shop/user/User.java
+++ web-project-shop-template/src/main/java/com/itwill/shop/user/User.java
@@ -1,5 +1,9 @@
 package com.itwill.shop.user;
 
+import java.util.List;
+
+import com.itwill.shop.order.Order;
+
 /*
  *  사용자관리를 위하여 필요한 도메인클래스(VO,DTO)
  *  USERINFO 테이블의 각컬럼에해당하는 멤버를 가지고있다
@@ -20,7 +24,8 @@ public class User {
 	private String password;
 	private String name;
 	private String email;
-
+	private List<Order> orderList;
+	
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
